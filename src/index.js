ac1397f2b3d164ac73f1a2dc4e9208cc52731836
kellyxc28 <kellyxiongc@gmail.com>
2024-05-16 20:13:12 -0400

    Completed Question 10: Getting our Values - Username

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@
 
 const handleSubmit = (e) => {
   e.preventDefault();
+
+  const form = e.target;
+
+  const usernameValue = form.username.value;
+  const usernameResults = document.querySelector('#results-username');
+  usernameResults.textContent = usernameValue;
 };
 
 const submitButton = document.querySelector('#new-user-form');
