88a94ef78c8ec8cd60ea701d5a71575e7e9ce34b
kellyxc28 <kellyxiongc@gmail.com>
2024-05-16 20:37:44 -0400

    Completed Question 10: Getting our Values - didLikeAssignment

--- src/index.js
+++ src/index.js
@@ -10,6 +10,24 @@ const handleSubmit = (e) => {
   const usernameValue = form.username.value;
   const usernameResults = document.querySelector('#results-username');
   usernameResults.textContent = usernameValue;
+
+  const codingLevelValue = form.codingLevel.value;
+  const codingLevelResults = document.querySelector('#results-coding-level');
+  codingLevelResults.textContent = codingLevelValue;
+
+  const locationValue = form.location.value;
+  const locationResults = document.querySelector('#results-location');
+  locationResults.textContent = locationValue;
+
+  const likingAssignmentValue = form.didLikeAssignment.checked;
+  const likingAssignmentResults = document.querySelector(
+    '#results-did-like-assignment'
+  );
+  if (likingAssignmentValue) {
+    likingAssignmentResults.textContent = 'Yes';
+  } else {
+    likingAssignmentResults.textContent = 'No';
+  }
 };
 
 const submitButton = document.querySelector('#new-user-form');
